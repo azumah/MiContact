07177a4429c99ae5f853a89e91cf5806f5775c2e
Nelson Klutse <nelson.klutse@meltwater.com>
2020-04-28 12:20:07 +0000

    Initial commit for MiContact demo app

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MiContact());
+}
+
+class MiContact extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(color: Colors.yellow, width: 100, height: 100),
+                Container(color: Colors.green, width: 100, height: 100),
+              ],
+            ),
+            Container(color: Colors.blue, width: 100),
+          ],
+        )),
+      ),
+    );
+  }
+}
