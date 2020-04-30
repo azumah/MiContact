d9205ede69ecaafe9589cc99a4eddae5716f4e90
Nelson Klutse <nelson.klutse@meltwater.com>
2020-04-30 17:33:43 +0000

    center job profile

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MiContact extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal[500],
+        backgroundColor: Colors.teal.shade700,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 40,
@@ -32,6 +33,48 @@ class MiContact extends StatelessWidget {
                     fontSize: 14.0,
                     fontFamily: 'SourceSansPro',
                     fontWeight: FontWeight.normal),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade700,
+                  ),
+                  title: Text(
+                    '+233 55 3400 172',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade700,
+                  ),
+                  title: Text(
+                    'nelsonklutsey@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
